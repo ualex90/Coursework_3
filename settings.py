3e58f262ad8cc58d9750c571f6b9a72d759a40cf
u_alex90 <u_alex90@mail.ru>
2023-07-05 21:02:54 +1000

    Reorganized project structure

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 # Папки проекта
 ROOT = Path(__file__).resolve().parent
-DATA = Path(ROOT, "data")
+SRC = Path(ROOT, "src")
+DATA = Path(SRC, "data")
 
 # Файлы данных
 OPERATIONS = Path(DATA, "operations.json")
