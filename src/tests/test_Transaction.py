7c7c14bba4ef95284080da609caf1cf7f62cdce9
u_alex90 <u_alex90@mail.ru>
2023-07-09 12:05:19 +1000

    fixed test_Transaction.py

--- src/tests/test_Transaction.py
+++ src/tests/test_Transaction.py
@@ -1,19 +1,23 @@
-from src.transaction import Transaction
+import pytest
+
+from src.transaction.Transaction import Transaction
 
 
-def test_Transaction_init(tr_fixture):
+def test_Transaction_init(mod_tr_fixture):
+    tr = Transaction(**mod_tr_fixture[0])
+    assert tr.id_ == 11111111
+    assert tr.state == 'EXECUTED'
+    assert str(tr.date) == '2023-07-07 10:00:00.111111'
+    assert tr.currency_name == 'руб.'
+    assert tr.amount == '10000.99'
+    assert tr.currency_name == 'руб.'
+    assert tr.currency_code == 'RUB'
+    assert tr.description == 'Перевод организации'
+    assert tr.from_ == 'Maestro 1234123412341234'
+    assert tr.to_ == 'Счет 12345123451234512345'
 
-    assert tr_fixture.id_ == 11111111
-    assert tr_fixture.state == 'EXECUTED'
-    assert str(tr_fixture.date) == '2023-07-07 10:00:00.111111'
-    assert tr_fixture.currency_name == 'руб.'
-    assert tr_fixture.amount == '10000.99'
-    assert tr_fixture.currency_name == 'руб.'
-    assert tr_fixture.currency_code == 'RUB'
-    assert tr_fixture.description == 'Перевод организации'
-    assert tr_fixture.from_ == 'Maestro 1234123412341234'
-    assert tr_fixture.to_ == 'Счет 12345123451234512345'
 
-
-def test_Transaction_str(tr_fixture):
-    ...
+@pytest.mark.parametrize('index', [0, 1, 2])
+def test_Transaction_str(mod_tr_fixture, result_fixture, index):
+    data = mod_tr_fixture[index]
+    assert Transaction(**data).__str__() == result_fixture[index]
